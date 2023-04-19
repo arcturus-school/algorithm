2f3ae8433c11d612100f4faed0b080aa568fdb3f
ARCTRUS <1638330246@qq.com>
2023-04-19 22:01:38 +0800

    feat: Add Test class

--- lab/lab/index.cpp
+++ lab/lab/index.cpp
@@ -2,15 +2,43 @@
 #include "output.h"
 #include "lab.h"
 
+class Test {
+    std::map<std::string, void(*)()> map;
+    void(*cur)()  = nullptr;
+public:
+    void add(const std::string& key, void (*func)()) {
+        map[key] = func;
+    }
+
+    void set(const std::string& key) {
+        if (map.find(key) != map.end()) {
+            cur = map[key];
+        }
+        else {
+            cur = nullptr;
+        }
+    }
+
+    void operation() {
+        if (cur != nullptr) {
+            cur();
+        }
+    }
+};
+
 int main() {
+
+    Test test;
+
+    test.add("lab01", lab01);
+    test.add("lab02", lab02);
+    test.add("lab03", lab03);
+    test.add("lab04", lab04);
+
+    // 设置当前需要执行的方法
+    test.set("lab04");
     
-    // lab01();
-
-    // lab02();
-
-    lab03();
-
-    // lab04();
+    test.operation();
 
     return 0;
 }
