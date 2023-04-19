d2e3b29f328f3c4ef93ad2bcff0227caba80385a
ARCTRUS <1638330246@qq.com>
2023-04-19 22:00:04 +0800

    feat: Add print strings and format double to output function

--- lab/lab/output.h
+++ lab/lab/output.h
@@ -2,8 +2,16 @@
 
 #include <vector>
 
-inline void output(std::vector<int>& array, const char* sep = " ") {
+inline void output(std::vector<int>& array, const std::string& sep = " ") {
     for (int i = 0; i < array.size(); i++) {
         std::cout << array[i] << sep;
     }
+}
+
+inline void output(const std::string& str, const std::string& sep = "\n") {
+    std::cout << str << sep;
+}
+
+inline void output(const double num, const int fix, const std::string& sep = "\n") {
+    std::cout << std::fixed << std::setprecision(fix) << num << sep;
 }
