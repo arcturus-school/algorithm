c7c03f08a69c35b31e7462535a96e6be01300b02
ARCTRUS <1638330246@qq.com>
2023-04-20 10:43:17 +0800

    feat: Add output numbers function

--- lab/lab/output.h
+++ lab/lab/output.h
@@ -15,3 +15,7 @@ inline void output(const std::string& str, const std::string& sep = "\n") {
 inline void output(const double num, const int fix, const std::string& sep = "\n") {
     std::cout << std::fixed << std::setprecision(fix) << num << sep;
 }
+
+inline void output(const int num, const std::string& sep = "") {
+    std::cout << num << sep;
+}
